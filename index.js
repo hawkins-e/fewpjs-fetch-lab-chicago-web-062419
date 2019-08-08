3e8854a86f9d0f3c82ef07a9b96bcd553ae98401
Erica Hawkins <ehawk901@gmail.com>
2019-08-08 13:09:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -10,6 +12,18 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+// function find_5th_Book(, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     title: 'The Fifth Season',
+//     author: 'N.K. Jemisin',
+//     img: 'https://images-na.ssl-images-amazon.com/images/I/61XfS2XCw3L._SL160_SX135_.jpg'
+//   })
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
